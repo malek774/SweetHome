c49ea4ab420c8a9e372018033dfe2519d0ccb047
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-04-29 14:34:36 +0200

    getspotifyKey function works

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -6,9 +6,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpService {
 
+  authEndPoint = "https://accounts.spotify.com/authorize";
+  clientId = "edb6db7c1c604795b872fe40255d52fc";
+  redirectUri = 'http://localhost:4200';
+
+
+
+  Oauth = `https://accounts.spotify.com/authorize?client_id=21e0d925502047b08c82d197558a42e5&redirect_uri=http:%2F%2F127.0.0.1:4200%2F&scope=user-read-private%20user-read-email%20streaming&response_type=token&state=123`;
+
   constructor(private http: HttpClient) { }
 
   getSpotifyKey() {
-    return this.http.get();
+    return window.open(this.Oauth);
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http: HttpService) {
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  openLoginPage() {
+    this._http.getSpotifyKey();
   }
 
 }
