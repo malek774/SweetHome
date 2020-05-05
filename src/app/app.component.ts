ba7697f8f3ec104b94be0e5446afb566fa7b3e42
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-05-05 11:51:15 +0200

    deleted oninit lifecycle and other unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
-import { Component, OnInit, DoCheck, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  spotifyKey: string;
+export class AppComponent {
 
-  constructor() {
-
-  }
-
-  ngOnInit() {
-    this.spotifyKey = this.route.snapshot.fragment.split('&')[0].substr(13);
-    console.log(this.spotifyKey);
-  }
 }
