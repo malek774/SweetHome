14032ac222492bc917bd8665fd49e8f33f7a817c
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-05-14 16:04:29 +0200

    busy with adding all the API endpoints

--- src/app/spotifyweb.service.ts
+++ src/app/spotifyweb.service.ts
@@ -46,57 +46,226 @@ export class SpotifywebService {
     });
   }
 
-  // fetchData(endPoint, method, params) {
-  //   this.setHeaders(this.headers); //the header with the key should be set everytime you do a fetch
-  //   return this.http.get(`${this.apiFirstPartURL}/${endPoint}`, {
-  //     headers: this.headers
-  //   });
-  // }
 
   fetchData(endPoint, method) {
     this.setHeaders(this.headers); //the header with the key should be set everytime you do a fetch
-    // return this.http.get(`${this.apiFirstPartURL}/${endPoint}`, {
-    //   headers: this.headers
-    // });
-
     return this.http.request(method, `${this.apiFirstPartURL}/${endPoint}`, {
       headers: this.headers
     });
   }
 
-  //ALBUMS
+  //********************ALBUMS********************//
 
-  // getAlbums() {
-  //   return this.fetchData("albums");
-  // }
+  //Get Spotify catalog information for a single album.
+  getAlbums(id: string) {
+    return this.fetchData(`albums/${id}`, 'GET');
+  }
 
-  // getAlbumTracks() {
-  //   return this.fetchData("albums");
-  // }
+  //Get Spotify catalog information about an album’s tracks. Optional parameters can be used to limit the number of tracks returned.
+  getSeveralAlbums(id: string[]) {
+    return this.fetchData(`albums/${id.join(',')}`, 'GET');
+  }
 
-  //ARTISTS
+  //Get Spotify catalog information for multiple albums identified by their Spotify IDs.
+  getAlbumTracks(id: string) {
+    return this.fetchData(`albums/${id}/tracks`, 'GET');
+  }
 
-  //BROWSE
+  //********************ARTIST********************//
 
-  //EPISODES
+  //Get Spotify catalog information for a single artist identified by their unique Spotify ID.
+  getArtist(id: string) {
+    return this.fetchData(`artists/${id}`, 'GET');
+  }
 
-  //FOLLOW
+  //Get Spotify catalog information about an artist’s albums. Optional parameters can be specified in the query string to filter and sort the response.
+  getArtistsAlbum(id: string) {
+    return this.fetchData(`artists/${id}/albums`, 'GET');
+  }
 
-  //LIBRARY
+  //Get Spotify catalog information about an artist’s top tracks by country.
+  getArtistsTopTracks(id: string) {
+    return this.fetchData(`artists/${id}/top-tracks`, 'GET');
+  }
 
-  //PERSONALIZATION
+  //Get Spotify catalog information about artists similar to a given artist. Similarity is based on analysis of the Spotify community’s listening history.
+  getArtistsRelatedArtists(id: string) {
+    return this.fetchData(`artists/${id}/related-artists`, 'GET');
+  }
 
-  //PLAYER
+  //Get Spotify catalog information for several artists based on their Spotify IDs.
+  getSeveralArtists(id: string[]) {
+    return this.fetchData(`artists/${id.join(',')}/artists`, 'GET');
+  }
 
-  //PLAYLISTS
+  //********************BROWSE********************//
 
-  //SEARCH
+  //Get a single category used to tag items in Spotify (on, for example, the Spotify player’s “Browse” tab)
+  getCategory(id: string) {
+    return this.fetchData(`browse/categories/${id}`, 'GET');
+  }
 
-  //TRACKS
+  //Get a list of Spotify playlists tagged with a particular category.
+  getCategoryPlaylist(id: string) {
+    return this.fetchData(`browse/categories/${id}/playlists`, 'GET');
+  }
 
-  //SHOWS
+  //Get a list of categories used to tag items in Spotify (on, for example, the Spotify player’s “Browse” tab).
+  getCategories() {
+    return this.fetchData(`browse/categories`, 'GET');
+  }
 
-  //USERS PROFILE
+  //Get a list of Spotify featured playlists (shown, for example, on a Spotify player’s ‘Browse’ tab).
+  getFeaturedPlaylists() {
+    return this.fetchData(`browse/featured-playlists`, 'GET');
+  }
+
+  //Get a list of new album releases featured in Spotify (shown, for example, on a Spotify player’s “Browse” tab).
+  getNewAlbumReleases() {
+    return this.fetchData(`browse/new-releases`, 'GET');
+  }
+
+  //Create a playlist-style listening experience based on seed artists, tracks and genres
+  getRecommendations() {
+    return this.fetchData(`recommendations`, 'GET');
+  }
+
+  //********************EPISODES********************//
+
+  //Get Spotify catalog information for a single episode identified by its unique Spotify ID.
+  getEpisode(id) {
+    return this.fetchData(`episode/${id}`, 'GET');
+  }
+
+  //Get Spotify catalog information for multiple episodes based on their Spotify IDs.
+  getSeveralEpisodes(id: string[]) {
+    return this.fetchData(`episode/${id}`, 'GET');
+  }
+
+  //********************FOLLOW********************//
+
+  //Check to see if the current user is following one or more artists or other Spotify users.
+  getCheckUserFollowingArtistsOrUsers(type: string, id: string[]) {
+    return this.fetchData(`me/following/contains?type=${type}&ids=${id.join(',')}`, 'GET');
+  }
+
+  //Check to see if one or more Spotify users are following a specified playlist.
+  getCheckUsersFollowPlaylist(usersId: string[], playlistId: string) {
+    return this.fetchData(`playlists/${playlistId}/followers/contains?ids=${usersId.join(',')}`, 'GET');
+  }
+
+  //Add the current user as a follower of one or more artists or other Spotify users.
+  followArtistsOrUsers(type: string, id: string[]) {
+    return this.fetchData(`me/following?type=${type}&ids=${id.join(',')}`, 'PUT');
+  }
+
+  // Add the current user as a follower of a playlist.
+  followPlaylist(playlistId: string) {
+    return this.fetchData(`playlists/${playlistId}/followers`, 'PUT');
+  }
+
+  //Get the current user’s followed artists.
+  getUsersFollowedArtists(type: string) {
+    return this.fetchData(`me/following?type=${type}`, 'GET');
+  }
+
+  //Remove the current user as a follower of one or more artists or other Spotify users.
+  unfollowArtistsOrUsers(type: string, id: string[]) {
+    return this.fetchData(`me/following?type=${type}&ids=${id.join(',')}`, 'DELETE');
+  }
+
+  //Remove the current user as a follower of a playlist. 
+  unfollowPlaylist(playlistId: string) {
+    return this.fetchData(`playlists/${playlistId}/followers`, 'DELETE');
+  }
+
+  //********************LIBRARY********************//
+
+  //This is used for users that have their own tracks/albums which for the time being will not be implemented
+
+  //********************PERSONALIZATION********************//
+
+  //Get the current user’s top artists or tracks based on calculated affinity.
+  getUsersTopArtistsAndTracks(type: string) {
+    return this.fetchData(`me/top/${type}`, 'GET');
+  }
+
+  //********************PLAYER********************//
+
+  //Get information about the user’s current playback state, including track or episode, progress, and active device.
+  getInfoUserCurrentPlayback() {
+    return this.fetchData(`me/player`, 'GET');
+  }
+
+  //Get tracks from the current user’s recently played tracks.
+  getTracksUsersRecentPlayback() {
+    return this.fetchData(`me/player/recently-played`, 'GET');
+  }
+
+  //Get the object currently being played on the user’s Spotify account.
+  getTracksUsersCurrentPlayback() {
+    return this.fetchData(`me/player/currently-played`, 'GET');
+  }
+
+  //Pause playback on the user’s account.
+  pauseUsersPlayback() {
+    return this.fetchData(`me/player/pause`, 'PUT');
+  }
+
+  //Seeks to the given position in the user’s currently playing track.
+  seekPositionCurrentlyPlayTrack(time) {
+    return this.fetchData(`me/player/seek?position_ms=${time}`, 'PUT');
+  }
+
+  //Set the repeat mode for the user’s playback. Options are repeat-track, repeat-context, and off.
+  setRepeatModeOnUsersPlayback(state) {
+    return this.fetchData(`me/player/repeat?state=${state}`, 'PUT');
+  }
+
+  //Set the volume for the user’s current playback device.
+  setVolumeUsersPlayback(volume: number) {
+    return this.fetchData(`me/player/volume?volume_percent=${volume}`, 'PUT');
+  }
+
+  //Skips to next track in the user’s queue.
+  skipToNextTrack() {
+    return this.fetchData(`me/player/next`, 'POST');
+  }
+
+  //Skips to previous track in the user’s queue.
+  skipToPreviousTrack() {
+    return this.fetchData(`me/player/previous`, 'POST');
+  }
+
+  //Start a new context or resume current playback on the user’s active device.
+  startResumeUserPlayback() {
+    return this.fetchData(`me/player/play`, 'PUT');
+  }
+
+  //Toggle shuffle on or off for user’s playback.
+  toggleShuffleUsersPlayback(state) {
+    return this.fetchData(`me/player/shuffle?state=${state}`, 'PUT');
+  }
+
+  //********************PLAYLISTS********************//
+
+  //Add one or more items to a user’s playlist.
+  addItemsToPlaylist(playlistId: string) {
+    return this.fetchData(`playlists/${playlistId}/tracks`, 'POST');
+  }
+
+  //Change a playlist’s name and public/private state. (The user must, of course, own the playlist.)
+  changePlaylistDetails(playlistId: string) {
+    return this.fetchData(`playlists/${playlistId}`, 'PUT');
+  } //need to add body parameters
+
+  //********************SEARCH********************//
+
+  //********************TRACKS********************//
+
+  //********************SHOWS********************//
+
+  //********************USERS PROFILE********************//
 
 
   searchItem(query) { //, type, market, limit, offset
@@ -107,16 +276,7 @@ export class SpotifywebService {
     });
   }
 
-  getCategoryPlaylist(categoryId) {
-    this.setHeaders(this.headers);
-    return this.http.get(`https://api.spotify.com/v1/browse/categories/${categoryId}/playlists`, {
-      headers: this.headers
-    });
-  }
 
-  getCategories() {
-    return this.fetchData("browse/categories", "GET");
-  }
 
   getUserPlaylist() {
     return this.fetchData("me/playlists", 'GET');
